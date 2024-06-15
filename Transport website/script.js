34f571925a45389cb94ab881967e9cbf29cc05b4
omkar4255 <kumaromkar015@gmail.com>
2024-06-15 17:25:31 +0530

    add code from starting and delete previous code

---
+++ Transport website/script.js
@@ -0,0 +1,4 @@
+ // JavaScript to toggle the navigation links on small screens
+ document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".nav-links").classList.toggle("active");
+});
